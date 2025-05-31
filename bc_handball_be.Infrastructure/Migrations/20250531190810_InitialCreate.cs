f1152a9346a65dc79c48e12767dd1cebc5b1f2f2
Tomasp117 <tomasp117@seznam.cz>
2025-05-31 21:14:39 +0200

    DockerCompose

---
+++ bc_handball_be.Infrastructure/Migrations/20250531190810_InitialCreate.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace bc_handball_be.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitialCreate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}

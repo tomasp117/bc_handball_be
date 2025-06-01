90d4e9fbfd61d13ddecd4e9ae34bc730f0352378
Tomasp117 <tomasp117@seznam.cz>
2025-06-01 13:04:38 +0200

    dto small update

--- bc_handball_be.API/DTOs/RegisterDTO.cs
+++ bc_handball_be.API/DTOs/RegisterDTO.cs
@@ -7,7 +7,7 @@
         public string Email { get; set; } = string.Empty;
         public string PhoneNumber { get; set; } = string.Empty;
         public string Address { get; set; } = string.Empty;
-        public DateTime DateOfBirth { get; set; }
+        public DateTime DateOfBirth { get; set; } = DateTime.MinValue;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Role { get; set; } = string.Empty;
